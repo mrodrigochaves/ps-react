0d37feeefa1dd55ef2bbef7c023a32160299ec1a
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-18 12:32:29 -0300

    atualizando tabela

--- src/components/DataTable/index.tsx
+++ src/components/DataTable/index.tsx
@@ -1,55 +1,72 @@
+import { Table, TableCaption, TableContainer, Thead, Tr, Th, Tbody, Td, Tfoot } from "@chakra-ui/react";
+import { CSSProperties } from "react";
+import { tableStyles } from "./styles";
 
-import { Container,Table, TableCaption, TableContainer, Thead, Tr, Th, Tbody, Td, Tfoot } from "@chakra-ui/react";
+interface TableCellProps {
+  isNumeric?: boolean;
+  children?: React.ReactNode;
+}
 
+function TableCell({ isNumeric, children }: TableCellProps) {
+  const cellStyles: CSSProperties = {
+    ...tableStyles.solidBorder,
+  };
+
+  return (
+    <Td style={cellStyles} isNumeric={isNumeric}>
+      {children}
+    </Td>
+  );
+}
 
 export function DataTable() {
   return (
-    
-      <div>
-        <Container maxW='4xl' centerContent>
-        <TableContainer>
-          <Table variant='simple'>
-            <TableCaption>Informações sobre conta dos clientes</TableCaption>
-            <Thead>
-              <Tr>
-                <Th>Dados</Th>
-                <Th isNumeric>Valência</Th>
-                <Th>Tipo</Th>
-                <Th>Nome do operador transacionado</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>14/02/2019</Td>
-                <Td isNumeric>R$ 30895,46</Td>
-                <Td>depósito</Td>
-                <Td >25.4</Td>
-              </Tr>
-              <Tr>
-                <Td>12/04/2019</Td>
-                <Td isNumeric>R$ 12,24</Td>
-                <Td>Transferência Entrada</Td>
-                <Td >30.48</Td>
-              </Tr>
-              <Tr>
-                <Td>11/06/2020</Td>
-                <Td isNumeric>R$ -500,50</Td>
-                <Td>Transferência de Saída</Td>
-                <Td >0.91444</Td>
-              </Tr>
-            </Tbody>
-            <Tfoot>
-              <Tr>
-                <Td>11/06/2020</Td>
-                <Td isNumeric>R$ -1234,00</Td>
-                <Td>Saque</Td>
-                <Td >0.91444</Td>
-              </Tr>
-            </Tfoot>
-          </Table>
-        </TableContainer>
-        </Container>
-      </div>
-  
+    <TableContainer padding="2%">
+      <Table variant="simple">
+        <TableCaption>Informações sobre conta dos clientes</TableCaption>
+        <Thead>
+          <Tr border={'solid 1px #000'} >
+            <Th borderBottom={'solid 1px #000'} textColor={'#000'}>Saldo Total: R$ </Th>
+            <Th borderBottom={'solid 1px #000'} textColor={'#000'}>Saldo no periodo: R$ </Th>
+          </Tr>
+        </Thead>
+        <Thead>
+          <Tr>
+            <TableCell>Dados</TableCell>
+            <TableCell isNumeric>Valência</TableCell>
+            <TableCell>Tipo</TableCell>
+            <TableCell>Nome do operador transacionado</TableCell>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <TableCell>14/02/2019</TableCell>
+            <TableCell isNumeric>R$ 30895,46</TableCell>
+            <TableCell>depósito</TableCell>
+            <TableCell></TableCell>
+          </Tr>
+          <Tr>
+            <TableCell>12/04/2019</TableCell>
+            <TableCell isNumeric>R$ 12,24</TableCell>
+            <TableCell>Transferência Entrada</TableCell>
+            <TableCell>Fulano</TableCell>
+          </Tr>
+          <Tr>
+            <TableCell>11/06/2020</TableCell>
+            <TableCell isNumeric>R$ -500,50</TableCell>
+            <TableCell>Transferência de Saída</TableCell>
+            <TableCell>Sicrano</TableCell>
+          </Tr>
+        </Tbody>
+        <Tfoot>
+          <Tr>
+            <TableCell>11/06/2020</TableCell>
+            <TableCell isNumeric>R$ -1234,00</TableCell>
+            <TableCell>Saque</TableCell>
+            <TableCell></TableCell>
+          </Tr>
+        </Tfoot>
+      </Table>
+    </TableContainer>
   );
 }
