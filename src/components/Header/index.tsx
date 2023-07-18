78a36af4018c412e3b488a6f75281406d43503a7
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-18 14:35:34 -0300

    estilizando Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,32 +1,29 @@
-import { Input, Stack, Text } from "@chakra-ui/react"
+import { Input, Stack, Text, Box } from "@chakra-ui/react";
+import { inputStyles, containerStyles, labelStyles } from "./styles";
 
 export function Header() {
-    return (
-
-        <Stack direction={"row"} spacing={3} padding={'2%'}>
-            <Text marginLeft={'5%'}>Data de Inicio</Text>
-            <Input
-                borderColor={'none'}
-                placeholder="Select Date and Time"
-                size="md"
-                type="date"
-            />
-            <Text marginLeft={'5%'}>Data de Fim</Text>
-            <Input
-                borderColor={'none'}
-                marginLeft={'5%'}
-                placeholder="Select Date and Time"
-                size="md"
-                type="date"
-            />
-            <Text marginLeft={'5%'}>Nome do operador transacionado de Inicio</Text>
-            <Input
-                borderColor={'none'}
-                marginLeft={'5%'}
-                placeholder='medium size'
-                _placeholder={{ color: 'inherit' }}
-                size='md' />
-        </Stack>
-
-    );
+  return (
+    <Stack flexDirection={'row'} padding="3%" justifyContent={'space-between'}>
+      <Box  style={containerStyles}>
+        <Text style={labelStyles}>Data de Inicio</Text>
+        <Input style={inputStyles} size="md" type="date" />
+      </Box>
+      <Box  style={containerStyles}>
+        <Text style={labelStyles}>Data de Fim</Text>
+        <Input
+          style={inputStyles}
+          size="md"
+          type="date"
+        />
+      </Box>
+      <Box  style={containerStyles}>
+        <Text style={labelStyles}>Nome do operador transacionado</Text>
+        <Input
+          style={inputStyles}
+          _placeholder={{ color: 'inherit' }}
+          size="md"
+        />
+      </Box>
+    </Stack>
+  );
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,17 @@
+import { CSSProperties } from "react";
+
+export const inputStyles: CSSProperties = {
+  borderColor: '#000000',
+  borderRadius: '0',
+};
+
+export const containerStyles: CSSProperties = {
+
+  display: 'flex',
+  justifyContent: 'flex-end',
+  flexDirection: 'column',
+};
+
+export const labelStyles: CSSProperties = {  
+  marginBottom: 'auto',
+};
