2fa24115634ad1fc6e2e968d14d10bd2b2fe0555
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-18 20:06:27 -0300

    atualizando imports no home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,11 @@
 
 import { ChakraProvider, Container } from "@chakra-ui/react";
 import { DataTable } from "../../components/DataTable";
-import { Header } from "../../components/Header";
-import { Search } from "../../components/Search";
+
 export default function Home() {
   return (
     <ChakraProvider >
       <Container maxW='85%'>
-      <Header />
-      <Search />
       <DataTable />
       </Container>
     </ChakraProvider>
