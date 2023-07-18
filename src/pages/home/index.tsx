0dbc7d324be93771ccacd0f524b156138ec71525
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-18 12:33:58 -0300

    adicionando componentes

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,16 @@
 
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Container } from "@chakra-ui/react";
 import { DataTable } from "../../components/DataTable";
 import { Header } from "../../components/Header";
+import { Search } from "../../components/Search";
 export default function Home() {
   return (
-    <ChakraProvider>
+    <ChakraProvider >
+      <Container marginTop={'5%'} maxW='85%' bg='green.400' >
       <Header />
+      <Search />
       <DataTable />
+      </Container>
     </ChakraProvider>
   );
 }
