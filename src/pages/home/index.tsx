0ee91392d54e36d65e5816226b9cdcdd016ce4fe
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-18 01:06:07 -0300

    importando tabela

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,53 +1,12 @@
 
-import { ChakraProvider, Table, TableCaption, TableContainer, Thead, Tr, Th, Tbody, Td, Tfoot } from "@chakra-ui/react";
-
-
+import { ChakraProvider } from "@chakra-ui/react";
+import { DataTable } from "../../components/DataTable";
+import { Header } from "../../components/Header";
 export default function Home() {
   return (
     <ChakraProvider>
-      <div>
-        <TableContainer>
-          <Table variant='simple'>
-            <TableCaption>Informações sobre conta dos clientes</TableCaption>
-            <Thead>
-              <Tr>
-                <Th>Dados</Th>
-                <Th isNumeric>Valência</Th>
-                <Th>Tipo</Th>
-                <Th>Nome do operador transacionado</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>14/02/2019</Td>
-                <Td isNumeric>R$ 30895,46</Td>
-                <Td>depósito</Td>
-                <Td >25.4</Td>
-              </Tr>
-              <Tr>
-                <Td>12/04/2019</Td>
-                <Td isNumeric>R$ 12,24</Td>
-                <Td>Transferência Entrada</Td>
-                <Td >30.48</Td>
-              </Tr>
-              <Tr>
-                <Td>11/06/2020</Td>
-                <Td isNumeric>R$ -500,50</Td>
-                <Td>Transferência de Saída</Td>
-                <Td >0.91444</Td>
-              </Tr>
-            </Tbody>
-            <Tfoot>
-              <Tr>
-                <Td>11/06/2020</Td>
-                <Td isNumeric>R$ -1234,00</Td>
-                <Td>Saque</Td>
-                <Td >0.91444</Td>
-              </Tr>
-            </Tfoot>
-          </Table>
-        </TableContainer>
-      </div>
+      <Header />
+      <DataTable />
     </ChakraProvider>
   );
 }
